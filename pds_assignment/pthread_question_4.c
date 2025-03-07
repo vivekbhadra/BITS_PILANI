aee0393790987b488fcd0adc64a9e6ffaba187f8
Vivek Bhadra <vivek.bhadra@indra.co.uk>
2025-03-07 22:53:59 +0000

    Adding mode functionality.

--- pds_assignment/pthread_question_4.c
+++ pds_assignment/pthread_question_4.c
@@ -1,11 +1,54 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 pthread_mutex_t task_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  available = PTHREAD_COND_INITIALIZER;
 static int work = 0;
 static int die = 0;
+
+typedef struct node {
+    struct node * next;
+} node_t;
+
+node_t * head = NULL;
+
+void add_task()
+{
+    node_t * new_node = malloc(sizeof(node_t));
+    new_node->next = NULL;
+    printf("Head @%p\n", head);
+    printf("New task created at %p\n", new_node);
+    if(NULL == head) {
+        head = new_node;
+    } else {
+        node_t * temp = head;
+        node_t * prev = NULL;
+        while(temp)
+        {
+            // traverse through the list 
+            prev = temp;
+            temp = temp->next;
+        }
+        printf("Adding new node after %p\n", prev);
+        prev->next = new_node;
+    }
+}
+
+void remove_task()
+{
+    node_t * temp = head;
+    printf("Head at %p\n", head);
+    if(head != NULL)
+    {
+        head = head->next;
+        if(NULL != temp) {
+            printf("Removing task @%p\n", temp);
+            free(temp);
+        }
+    }
+}
 
 void * thread_function()
 {
@@ -17,7 +60,7 @@ void * thread_function()
             printf("pthread_mutex_lock failed\n");
         }
         
-        while(work == 0 && !die) {
+        while(head == NULL && !die) {
             printf("Thread %ld going to conditional wait\n", pthread_self());
             ret = pthread_cond_wait(&available, &task_lock);
             if (ret != 0) {
@@ -29,8 +72,8 @@ void * thread_function()
             pthread_mutex_unlock(&task_lock);
             break;
         }
-        if(work) {
-            --work; // do the work
+        if(head != NULL) {
+            remove_task(); // do the work
             printf("Work Done!\n");
         }
 
@@ -57,17 +100,18 @@ int main()
     }
 
     pthread_mutex_lock(&task_lock);
-    work = 4;
-    if(work != 0) {
-        printf("work remaining %d \n", work);
-        pthread_cond_broadcast(&available);
+    for(i=0; i < 4; ++i)
+    {
+        add_task();
     }
+    printf("work remaining\n");
+    pthread_cond_broadcast(&available);
     pthread_mutex_unlock(&task_lock);
 
     sleep(5);
 
     pthread_mutex_lock(&task_lock);
-    if(!work) {
+    if(head == NULL) {
         printf("All works done!\n");
         die = 1;
         pthread_cond_broadcast(&available);
