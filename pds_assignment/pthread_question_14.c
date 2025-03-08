d1d63a08821c8a23c8875714b9393463e7a449ef
Vivek Bhadra <vivek.bhadra@indra.co.uk>
2025-03-08 17:54:10 +0000

    Solution to PDS assignment question 14.

--- pds_assignment/pthread_question_14.c
+++ pds_assignment/pthread_question_14.c
@@ -1,19 +1,50 @@
 #include <stdio.h> 
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 pthread_mutex_t customer_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t available = PTHREAD_COND_INITIALIZER;
-static int stock = 10;
+static int stock = 0;
 
 void * thread_fucntion(void *arg)
 {
+    int ret = 0;
+    while(1)
+    {
+        ret = pthread_mutex_lock(&customer_lock);
+        if(ret != 0)
+        {
+            printf("pthread_mutex_lock() failed\n");
+            exit(1);
+        }
+        
+        while(stock == 0)
+        {
+            ret = pthread_cond_wait(&available, &customer_lock);
+            if (ret != 0)
+            {
+                printf("pthread_cond_wait() failed\n");
+            }
+        }
+
+        if(stock) 
+        {
+            --stock;
+            printf("Customer ID: %ld: Hurray!! Won discounted offer\n", pthread_self());
+        } else {
+            printf("Customer ID: %ld: Discounted price offer already closed\n", pthread_self());
+        }
+        pthread_mutex_unlock(&customer_lock);
+        break;
+    }
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
     int i = 0;
+    int ret = 0;
 
     if (argc < 2)
     {
@@ -24,13 +55,52 @@ int main(int argc, char **argv)
     }
 
     int num_customers = atoi(argv[1]);
+    pthread_t threads[num_customers];
+
     printf("NUmber of customers chosen by the user = %d\n", num_customers);
 
     for(int i=0; i < num_customers; ++i)
     {
+        ret = pthread_create(&threads[i], NULL, thread_fucntion, NULL);
+        if(ret != 0)
+        {
+            printf("pthread_create failed!\n");
+            exit(1);
+        }
+
 
     }
 
+    sleep(5);
+    ret = pthread_mutex_lock(&customer_lock);
+    if(ret != 0)
+    {
+        printf("pthread_mutex_lock() failed\n");
+        exit(1);
+    }
 
+    stock = 10; // 10 discounted price offer
+    ret = pthread_cond_broadcast(&available);
+    if(ret != 0)
+    {
+        printf("pthread_mutex_broadcast() failed\n");
+        exit(1);
+    }
+    ret = pthread_mutex_unlock(&customer_lock);
+    if(ret != 0)
+    {
+        printf("pthread_customer_unlock() failed\n");
+    }
 
+    for(i=0; i < num_customers; ++i)
+    {
+        ret = pthread_join(threads[i], NULL);
+        if(ret != 0)
+        {
+            printf("pthread_join() failed\n");
+            exit(1);
+        }
+    }
+
+    return 0;
 }
