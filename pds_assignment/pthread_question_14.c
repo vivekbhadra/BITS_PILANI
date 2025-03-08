badca5e1563ad6c3b3037c70f1c9ac253cbcd289
Vivek Bhadra <vivek.bhadra@indra.co.uk>
2025-03-08 18:39:40 +0000

    Fixing issue in main.

--- pds_assignment/pthread_question_14.c
+++ pds_assignment/pthread_question_14.c
@@ -6,8 +6,9 @@
 pthread_mutex_t customer_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t available = PTHREAD_COND_INITIALIZER;
 static int stock = 0;
+int offer_ends = 0;
 
-void * thread_fucntion(void *arg)
+void * thread_function(void *arg)
 {
     int ret = 0;
     while(1)
@@ -19,8 +20,9 @@ void * thread_fucntion(void *arg)
             exit(1);
         }
         
-        while(stock == 0)
+        while(stock == 0 && offer_ends != 1)
         {
+            printf("Customer [%ld] joining the waiting queue\n", pthread_self());
             ret = pthread_cond_wait(&available, &customer_lock);
             if (ret != 0)
             {
@@ -32,12 +34,17 @@ void * thread_fucntion(void *arg)
         {
             --stock;
             printf("Customer ID: %ld: Hurray!! Won discounted offer\n", pthread_self());
-        } else {
+        } 
+
+        if (offer_ends) {
             printf("Customer ID: %ld: Discounted price offer already closed\n", pthread_self());
+            pthread_mutex_unlock(&customer_lock);
+            break;
         }
         pthread_mutex_unlock(&customer_lock);
-        break;
     }
+
+    printf("Customer [%ld] returning main() home\n", pthread_self());
     return NULL;
 }
 
@@ -61,7 +68,7 @@ int main(int argc, char **argv)
 
     for(int i=0; i < num_customers; ++i)
     {
-        ret = pthread_create(&threads[i], NULL, thread_fucntion, NULL);
+        ret = pthread_create(&threads[i], NULL, thread_function, NULL);
         if(ret != 0)
         {
             printf("pthread_create failed!\n");
@@ -71,7 +78,7 @@ int main(int argc, char **argv)
 
     }
 
-    sleep(5);
+    sleep(2);
     ret = pthread_mutex_lock(&customer_lock);
     if(ret != 0)
     {
@@ -90,6 +97,29 @@ int main(int argc, char **argv)
     if(ret != 0)
     {
         printf("pthread_customer_unlock() failed\n");
+    }
+
+    sleep(5);
+
+    ret = pthread_mutex_lock(&customer_lock);
+    if(ret != 0)
+    {
+        printf("pthread_mutex_lock() failed\n");
+    }
+
+    if(stock == 0) {
+        printf("Looks like all 10 offers gone pretty quickly!\n");
+        offer_ends = 1;
+        ret = pthread_cond_broadcast(&available);
+        if(ret != 0)
+        {
+            printf("pthread_mutext_broadcast() failed\n");
+        }
+    }
+    ret = pthread_mutex_unlock(&customer_lock);
+    if(ret != 0)
+    {
+        printf("pthread_mutex_unlock() failed\n");
     }
 
     for(i=0; i < num_customers; ++i)
